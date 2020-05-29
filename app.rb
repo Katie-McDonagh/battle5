b2dbe705631b417153de6782b9a273d9c925d377
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-29 14:03:49 +0100

    add session

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
   # get '/' do
   # 'Testing infrastructure working!'
   # end
@@ -10,10 +11,17 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-  erb :play
-end
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
